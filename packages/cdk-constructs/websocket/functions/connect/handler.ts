c20a48419a519af0fdba20a4a8734cbdcce47826
Andre du Plessis <andredp@aleios.com>
2024-03-28 16:59:28 +0000

    feat: more entities

--- packages/cdk-constructs/websocket/functions/connect/handler.ts
+++ packages/cdk-constructs/websocket/functions/connect/handler.ts
@@ -9,13 +9,11 @@ import { WsConnectionEntity } from "../../dataModel/Connection";
 export const handler: APIGatewayProxyWebsocketHandlerV2 = async (
   event: APIGatewayProxyWebsocketEventV2
 ): Promise<APIGatewayProxyResultV2> => {
-  console.log("Event:", JSON.stringify(event));
-
   const connectionId = event.requestContext.connectionId;
-  const response = WsConnectionEntity.put({
+
+  await WsConnectionEntity.put({
     connectionId,
   });
 
-  console.log(`putCommand resp => ${JSON.stringify(response)}`);
   return { statusCode: 200 };
 };
--- packages/cdk-constructs/websocket/functions/demo/handler.ts
+++ packages/cdk-constructs/websocket/functions/demo/handler.ts
@@ -1,11 +1,13 @@
+import { getEnvVariable } from "@article-gpt/helpers";
 import {
   ApiGatewayManagementApiClient,
   PostToConnectionCommand,
 } from "@aws-sdk/client-apigatewaymanagementapi";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 import { TextEncoder } from "util";
+import { WsConnectionEntity } from "../../dataModel/Connection";
 
 const dynamoDBClient = new DynamoDBClient({
   region: process.env.AWS_REGION,
@@ -13,43 +15,28 @@ const dynamoDBClient = new DynamoDBClient({
 const dynamoDBDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
 
 const apiGwManApiClient = new ApiGatewayManagementApiClient({
-  region: process.env.AWS_REGION,
-  endpoint: process.env.WS_API_ENDPOINT,
+  region: getEnvVariable("AWS_REGION"),
+  endpoint: getEnvVariable("WS_API_ENDPOINT"),
 });
 
 export const handler = async function (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> {
-  console.log(`event => ${JSON.stringify(event)}`);
+  console.log(`Received event: ${JSON.stringify(event)}`);
 
-  const scanCommand = new ScanCommand({
-    TableName: process.env.CONN_TABLE_NAME,
-  });
-  const scanCommandResp = await dynamoDBDocClient.send(scanCommand);
-  console.log(`scanCommand resp => ${JSON.stringify(scanCommandResp)}`);
+  const entityScan = await WsConnectionEntity.scan();
+  const connectionItems = entityScan.Items || [];
 
   const textEncoder = new TextEncoder();
-  const connectionItems = scanCommandResp.Items || [];
   for (let ind = 0; ind < connectionItems.length; ind++) {
-    console.log(`connectionId => ${connectionItems[ind].connectionId}`);
-    try {
-      const postToConnectionCommandResp = await apiGwManApiClient.send(
-        new PostToConnectionCommand({
-          ConnectionId: connectionItems[ind].connectionId,
-          Data: textEncoder.encode(
-            "A new review published for book with id 123"
-          ),
-        })
-      );
-      console.log(
-        `postToConnectionCommand resp => ${JSON.stringify(
-          postToConnectionCommandResp
-        )}`
-      );
-    } catch (err) {
-      console.error(`Error while posting to connection => ${err}`);
-    }
+    await apiGwManApiClient.send(
+      new PostToConnectionCommand({
+        ConnectionId: connectionItems[ind].connectionId,
+        Data: textEncoder.encode("A new review published for book with id 123"),
+      })
+    );
   }
+
   return {
     statusCode: 200,
     body: JSON.stringify({
